8413d12d9500ee3bacb7b11b09529a5689dfd92f
Frederik Poulsen <frederik.hoejte.poulsen@invixo.com>
2024-06-17 14:26:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,31 +28,12 @@ module "kafka_topic" {
   confluent_cloud_api_key    = var.confluent_cloud_api_key
   confluent_cloud_api_secret = var.confluent_cloud_api_secret
   kafka_cluster_id           = var.kafka_cluster_id
-  topic_name                 = "demo-pipeline-topic"
-  partitions_count           = 3
-  rest_endpoint              = var.rest_endpoint
-  credentials_key            = var.credentials_key
-  credentials_secret         = var.credentials_secret
-  schema_file_path           = "./schemas/avro/tweet.avsc"
-  schema_registry_api_key    = var.schema_registry_api_key
-  schema_registry_api_secret = var.schema_registry_api_secret
-  schema_registry_cluster_id = var.schema_registry_cluster_id
-  schema_registry_rest_endpoint = var.schema_registry_rest_endpoint
-}
-
-# Create the resources
-module "kafka_topic" {
-  source = "./modules/kafka_topic"
-
-  confluent_cloud_api_key    = var.confluent_cloud_api_key
-  confluent_cloud_api_secret = var.confluent_cloud_api_secret
-  kafka_cluster_id           = var.kafka_cluster_id
   topic_name                 = "demo-pipeline-caja"
   partitions_count           = 6
   rest_endpoint              = var.rest_endpoint
   credentials_key            = var.credentials_key
   credentials_secret         = var.credentials_secret
-  schema_file_path           = "./schemas/avro/tweet.avsc"
+  schema_file_path           = "./schemas/avro/caja.avsc"
   schema_registry_api_key    = var.schema_registry_api_key
   schema_registry_api_secret = var.schema_registry_api_secret
   schema_registry_cluster_id = var.schema_registry_cluster_id
