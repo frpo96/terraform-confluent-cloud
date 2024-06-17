573edfa729b498c117592c18517ca8ef189e3c88
Frederik Poulsen <frederik.hoejte.poulsen@invixo.com>
2024-06-17 14:23:01 +0200

    caja makes new topic

--- main.tf
+++ main.tf
@@ -39,3 +39,22 @@ module "kafka_topic" {
   schema_registry_cluster_id = var.schema_registry_cluster_id
   schema_registry_rest_endpoint = var.schema_registry_rest_endpoint
 }
+
+# Create the resources
+module "kafka_topic" {
+  source = "./modules/kafka_topic"
+
+  confluent_cloud_api_key    = var.confluent_cloud_api_key
+  confluent_cloud_api_secret = var.confluent_cloud_api_secret
+  kafka_cluster_id           = var.kafka_cluster_id
+  topic_name                 = "demo-pipeline-caja"
+  partitions_count           = 6
+  rest_endpoint              = var.rest_endpoint
+  credentials_key            = var.credentials_key
+  credentials_secret         = var.credentials_secret
+  schema_file_path           = "./schemas/avro/tweet.avsc"
+  schema_registry_api_key    = var.schema_registry_api_key
+  schema_registry_api_secret = var.schema_registry_api_secret
+  schema_registry_cluster_id = var.schema_registry_cluster_id
+  schema_registry_rest_endpoint = var.schema_registry_rest_endpoint
+}
