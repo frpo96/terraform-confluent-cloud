db6406243acb54d461de3e0f01a4ac8d05dd48dc
Frederik Poulsen <frederik.hoejte.poulsen@invixo.com>
2024-06-20 16:52:05 +0200

    Update public_myapp_metadata_update.tf

--- topics/public_myapp_metadata_update.tf
+++ topics/public_myapp_metadata_update.tf
@@ -3,7 +3,7 @@ module "this" {
 
   common_vars = var.common_vars
 
-  topic_name       = "public_myapp_metadata_update"
+  topic_name       = "public_myapp_metadata_updates"
   partitions_count = 6
   schema_file_path = "./schemas/avro/caja.avsc"
 }
