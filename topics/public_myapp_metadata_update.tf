c8dd940c5a5961825316f971931bcce1046cfd1a
Frederik Poulsen <frederik.hoejte.poulsen@invixo.com>
2024-06-20 16:35:37 +0200

    test remove tag

--- topics/public_myapp_metadata_update.tf
+++ topics/public_myapp_metadata_update.tf
@@ -7,12 +7,3 @@ module "this" {
   partitions_count = 6
   schema_file_path = "./schemas/avro/caja.avsc"
 }
-
-module "this-tag-binding" {
-  source = "../modules/kafka_topic_tag_binding"
-
-  common_vars = var.common_vars
-
-  topic_name       = module.this.kafka_topic_name
-  tag_name         = "FINANCE"
-}
