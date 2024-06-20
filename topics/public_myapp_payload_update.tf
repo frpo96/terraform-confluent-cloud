1867d6ef1ec63ed3957bcf1ecd14925258d75247
Frederik Poulsen <frederik.hoejte.poulsen@invixo.com>
2024-06-20 16:10:19 +0200

    Create public_myapp_payload_update.tf

---
+++ topics/public_myapp_payload_update.tf
@@ -0,0 +1,18 @@
+module "this" {
+  source = "../modules/kafka_topic"
+
+  common_vars = var.common_vars
+
+  topic_name       = "public_myapp_payload_update"
+  partitions_count = 6
+  schema_file_path = "./schemas/avro/caja.avsc"
+}
+
+module "this-tag-binding" {
+  source = "../modules/kafka_topic_tag_binding"
+
+  common_vars = var.common_vars
+
+  topic_name       = module.this.kafka_topic_name
+  tag_name         = "FINANCE"
+}
